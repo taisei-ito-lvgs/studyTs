6940bfebd0276db8e3d9c590c04cba0d4585cd3e
taisei.ito <taisei.ito@leverages.jp>
2024-05-02 13:50:42 +0900

    add 3-11a

---
+++ 3-11a.ts
@@ -0,0 +1,5 @@
+function addNumbers(a: number, b: number) : number {
+    return a.toString() + b.toString();
+}
+//3-11a.ts:2:5 - error TS2322: Type 'string' is not assignable to type 'number'.
+//2     return a.toString() + b.toString();
